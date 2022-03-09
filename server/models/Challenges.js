4b8be30e1004a7eb5d7a7a80256905fd581dd548
Laura Gupta <lauragupta@gmail.com>
2022-03-08 23:28:01 -0800

    add Challenge model

---
+++ server/models/Challenges.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Challenge model
+const challengeSchema = new Schema(
+  {
+    challengeTitle: {
+      type: String, 
+      required: true,
+    },
+    challengeText: {
+      type: String, 
+      required: true,
+      minlength: 1, 
+      maxlength: 500,
+    },
+    createdAt: {
+      type: Date, 
+      default: Date.now,
+      immutable: true,
+    },
+    expiresAt: {
+      type: Date,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    challengers: [Challenger],
+  },
+  {
+    toJSON: {
+    virtuals: true,
+    },
+  }
+);
+
+ // Create a virtual property `numberOfChallengers` that gets the number of reactions associated with a thought
+challengeSchema.virtual('numberOfChallengers').get(function () {
+  return this.challengers.length;
+});
+
+//Virtual property for date
+challengeSchema.virtual('date').get(function() {
+  return this.createdAt.toDateString('en-US');
+});
+
+// Initialize Challenge model
+const Challenge= model('challenge', challengeSchema);
+
+module.exports = Challenge;
