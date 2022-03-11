fa8c3a70d6f1ce8abf2aab5f897c2a0ff094f47f
Laura Gupta <lauragupta@gmail.com>
2022-03-10 18:16:06 -0800

    add sign up form

--- client/src/Components/Pages/SignUp.js
+++ client/src/Components/Pages/SignUp.js
@@ -5,8 +5,24 @@ function SignUp() {
   return (
     <div>
       <h1>Sign up to Join a Challengeor to Create your own!</h1>
-      <form>
-        
+      <form className="form" id="signup-form">
+        <div className="form-group">
+          <label htmlFor="signupUsername">username:</label>
+          <input className="form-control" type="name" id="signupUsername" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="signupEmail">email:</label>
+          <input className="form-control" type="email" id="signupEmail" />
+          <div id="emailHelp" className="form-text">We'll never share your email with
+            anyone else.</div>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="signupPassword">password:</label>
+          <input className="form-control" type="password" id="signupPassword" />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit">Signup</button>
+        </div>
       </form>
     </div>
   )
