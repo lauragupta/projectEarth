8b3ff74e02316445996724f5eed4184d8518cb4f
Laura Gupta <lauragupta@gmail.com>
2022-03-10 18:25:36 -0800

    add Log In form to logInPage

--- client/src/Components/Pages/LogIn.js
+++ client/src/Components/Pages/LogIn.js
@@ -5,8 +5,18 @@ function LogIn() {
   return (
     <div>
       <h1>Log Back in to see your Challenges or to Create your own!</h1>
-      <form>
-        
+      <form className="form" id="signup-form">
+        <div className="form-group">
+          <label htmlFor="logInEmail">email:</label>
+          <input className="form-control" type="email" id="logInEmail" />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="logInPassword">password:</label>
+          <input className="form-control" type="password" id="logInPassword" />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit">Log In</button>
+        </div>
       </form>
     </div>
   )
