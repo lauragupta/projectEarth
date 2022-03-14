541f8deb1b3b2097ed4c6c73738db9e564c4299d
Laura Gupta <lauragupta@gmail.com>
2022-03-14 14:35:30 -0700

    bring back the commented out challenges

--- client/src/Components/Pages/Challenges.js
+++ client/src/Components/Pages/Challenges.js
@@ -3,17 +3,17 @@ import ChallengeCard from '../ChallengeCard';
 
 
 function Challenges() {
-  // const [challenges, setChallenges] = useState(null);
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/api/challenges')
-  //   .then(response => response.json())
-  //   .then(data => setChallenges(data))
-  // }, []) 
+  const [challenges, setChallenges] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:3001/api/challenges')
+    .then(response => response.json())
+    .then(data => setChallenges(data))
+  }, []) 
 
   return (
     <div>
       <h1>Check out these Challenges! What can you do?</h1>
-      {/* {challenges ? challenges.map((challenge) => <ChallengeCard challenge={challenge} key={challenge.id} />) : <p>loading</p>}  */}
+      {challenges ? challenges.map((challenge) => <ChallengeCard challenge={challenge} key={challenge.id} />) : <p>loading</p>} 
     </div>
   )
 }
